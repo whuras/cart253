dbf2d43d707b0efd90f1ef6e334b214d1dc1351d
Unknown <wayne.stamand@hotmail.com>
2021-08-28 18:32:26 -0400

    Add background color and gradient rectangles

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -1,17 +1,39 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Drawing Experiment
+Wayne Huras ID 40074423
 
-Here is a description of this template p5 project.
+Experimenting with p5's drawing and color functions.
+Currently draws multiple rectangles.
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  background(127, 255, 200);
+
+  let n = 1000;
+  let a = 255 / n;
+
+  createCanvas(windowWidth, windowHeight);
+  background('#decbdf');
+
+  rectMode(CENTER);
+  noStroke();
+
+  for(let i = 0; i < n; i++){
+    if (a > 255) a = 255;
+    a += 10;
+    fill(200, 255, 200, a)
+    rect(windowWidth/2, windowHeight/2, windowWidth/(i+2), windowHeight/(i+2));
+  }
+
+  line(0, 0, windowWidth/4, windowHeight/4);
+  line(windowWidth*0.75, windowHeight*0.75, windowWidth, windowHeight);
+  line(windowWidth, 0, windowWidth*0.75, windowHeight*0.25);
+  line(windowWidth*0.25, windowHeight*0.75, 0, windowHeight);
+
 }
 
 // draw()
