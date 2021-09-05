4b26de232d34aa15e3471f10ba7002a200b9018f
Unknown <wayne.stamand@hotmail.com>
2021-09-05 14:21:08 -0400

    Add JS Object

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -8,20 +8,27 @@ Experimenting with p5's drawing and color functions.
 Currently draws multiple rectangles.
 **************************************************/
 
+let bgColor = '#decbdf';
+let n = 1000;
+let a = 255 / n;
+
+let circleObj = {
+  xPos: 200,
+  yPos: 250,
+  size: 200,
+  speed: 5
+};
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
-  let n = 1000;
-  let a = 255 / n;
-
   createCanvas(windowWidth, windowHeight);
-  background('#decbdf');
 
   rectMode(CENTER);
-  noStroke();
+  //noStroke();
 
+  /*
   for(let i = 0; i < n; i++){
     if (a > 255) a = 255;
     a += 10;
@@ -33,12 +40,20 @@ function setup() {
   line(windowWidth*0.75, windowHeight*0.75, windowWidth, windowHeight);
   line(windowWidth, 0, windowWidth*0.75, windowHeight*0.25);
   line(windowWidth*0.25, windowHeight*0.75, 0, windowHeight);
-
+  */
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(bgColor);
+
+  if(circleObj.xPos > windowWidth - circleObj.size / 2 || circleObj.xPos < 0 + circleObj.size / 2){
+    circleObj.speed *= -1;
+  }
+
+  circleObj.xPos += circleObj.speed;
+  ellipse(circleObj.xPos, circleObj.yPos, circleObj.size);
 
 }
