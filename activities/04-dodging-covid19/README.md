8e75944296a66aaa8ef47b01da7cd6e25396fefb
Unknown <wayne.stamand@hotmail.com>
2021-09-06 13:05:17 -0400

    A4: Setup
    Setup for Activity Dodging COVID19.

---
+++ activities/04-dodging-covid19/README.md
@@ -0,0 +1,2 @@
+# CART253 - Creative Computation I
+Course work for Concordia University CART253 - CREATIVE COMPUTATION I.
---
+++ activities/04-dodging-covid19/js/script.js
@@ -0,0 +1,32 @@
+/**
+Activity: Dodging COVID-19
+Wayne Huras
+ID: 40074423
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
