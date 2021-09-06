5a320d972e15ceeb43273b4c952fa9746c0fc257
Unknown <wayne.stamand@hotmail.com>
2021-09-06 14:41:44 -0400

    E2: Setup using A4: Dodge Covid19 as a base

---
+++ exercises/dodge-em/js/script.js
@@ -0,0 +1,108 @@
+/**
+Exercise: Dodge 'em!
+Wayne Huras
+ID: 40074423
+
+Brief:
+- Change the way the user controls their circle
+- Add at least one new if-statement (including at least an else) that changes the nature of the simulation
+- Change the way the simulation looks
+- Use at least one image
+
+Evaluation:
+- Runs and meets the brief
+- Has suitable commenting
+- Includes a starting commit and then a reasonable number of commits throughout the work that include descriptive messages about what was done. Messages should be prefixed by Ex:,
+- Uses good naming for added variables
+- Is well structured, with new code added in sensible places in sensible orders
+*/
+
+"use strict";
+
+let covid19 = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 0
+  }
+};
+
+let user = {
+  x: 250,
+  y: 250,
+  size: 100,
+  fill: 255
+};
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  covid19.y = random(0, height);
+  covid19.vx = covid19.speed;
+
+  noCursor();
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+  // Background display
+  background(0);
+  for(let i = 0; i < 100; i++){
+    let x = random(0, width);
+    let y = random(0, height);
+
+    push();
+    stroke(255);
+    point(x, y);
+    pop();
+  }
+
+
+  // Covid19 Circle movement
+  covid19.x += covid19.vx;
+  covid19.y += covid19.vy;
+
+  if(covid19.x > width){
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  }
+
+  // User movement
+  user.x = mouseX;
+  user.y = mouseY;
+
+  // Check for catching covid19
+  let d = dist(user.x, user.y,  covid19.x, covid19.y);
+  if(d <= covid19.size/2 + user.size/2){
+    noLoop();
+  }
+
+  // Covid19 Display
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  // User Display
+  fill(user.fill);
+  ellipse(user.x, user.y, user.size);
+
+}
