f751c0ada13deb06ca0ff90955bb12c48be01e67
Unknown <wayne.stamand@hotmail.com>
2021-09-06 13:23:15 -0400

    A4: Add Background and End Condition

--- activities/04-dodging-covid19/js/script.js
+++ activities/04-dodging-covid19/js/script.js
@@ -51,22 +51,43 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  // Background display
   background(0);
+  for(let i = 0; i < 100; i++){
+    let x = random(0, width);
+    let y = random(0, height);
 
+    push();
+    stroke(255);
+    point(x, y);
+    pop();
+  }
+
+
+  // Covid19 Circle movement
   covid19.x += covid19.vx;
   covid19.y += covid19.vy;
-
-  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-  ellipse(covid19.x, covid19.y, covid19.size);
 
   if(covid19.x > width){
     covid19.x = 0;
     covid19.y = random(0, height);
   }
 
+  // User movement
   user.x = mouseX;
   user.y = mouseY;
 
+  // Check for catching covid19
+  let d = dist(user.x, user.y,  covid19.x, covid19.y);
+  if(d <= covid19.size/2 + user.size/2){
+    noLoop();
+  }
+
+  // Covid19 Display
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+  // User Display
   fill(user.fill);
   ellipse(user.x, user.y, user.size);
 
