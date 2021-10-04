98002ef1ce66931ac7d46893cfe240d442d6ed34
Unknown <wayne.stamand@hotmail.com>
2021-10-04 11:32:52 -0400

    E3: State setup
    Create three states: start, sim, and end for the various stages of the simulation.

--- exercises/love-actaully/README.md
+++ exercises/love-actaully/README.md
@@ -1,3 +1,8 @@
 # CART253 - Creative Computation I, Exercise 3: Love, Actually!
+## Wayne Huras, ID 40074423
 
 Instructions:
+
+
+Credits:
+- timer: https://editor.p5js.org/marynotari/sketches/S1T2ZTMp-
--- exercises/love-actaully/js/script.js
+++ exercises/love-actaully/js/script.js
@@ -1,13 +1,34 @@
-/**
-Title of Project
-Author Name
+/**************************************************
+Exercise: Love, actually!
+Wayne Huras ID 40074423
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
+Brief:
+- Allow the user to control one of the circles
+- Make the non-user circle move differently
+- Add at least one extra function
+- Add at least one extra ending
+
+Evaluation:
+- Functional: The program runs and meets the brief in terms of functionality and code requirements, shows mastery of the course material
+- Stylish: Code follows the relevant parts of the style guide
+- Committed: Commits and messages follow the style guide
+- Creative: The program is an interesting and expressive experience for the user
+**************************************************/
 
 "use strict";
 
+let bgColor = undefined;
+let startBGColor = 'rgba(128,128,128,255)';
+let endBGColor = 'rgba(255,128,255,255)';
+
+let timer = 5;
+
+let state = undefined;
+const states = {
+  START: "start",
+  SIM: "sim",
+  SEND: "END"
+}
 
 /**
 Description of preload
@@ -21,7 +42,13 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  bgColor = startBGColor;
+  state = states.START;
+
+  textSize(64);
+  fill(255);
 }
 
 
@@ -29,5 +56,35 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(bgColor);
 
+  if(state == states.START){
+    startSim();
+  }
+  else if(state == states.END){
+    endSim();
+  }
+}
+
+function keyPressed(){
+  state = states.END;
+}
+
+function startSim(){
+  bgColor = startBGColor;
+  text("game started", width/2, height/2);
+}
+
+function endSim(){
+  bgColor = endBGColor;
+  text("game over", width/2, height/2);
+
+  // timer credit: https://editor.p5js.org/marynotari/sketches/S1T2ZTMp-
+  if(frameCount % 60 == 0 && timer > 0){
+    timer --;
+  }
+  if(timer <= 0){
+    timer = 0;
+    state = states.START;
+  }
 }
