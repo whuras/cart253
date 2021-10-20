4e5e06cb8c4cd15aa24abd093127bfe1a57b6445
Unknown <wayne.stamand@hotmail.com>
2021-10-20 11:10:32 -0400

    E4: Started Age of Aquariums
    Add initial Readme and comments

---
+++ exercises/age-of-aquariums/README.md
@@ -0,0 +1,8 @@
+# CART253 - Creative Computation I, Exercise 4: Age of Aquariums
+## Wayne Huras, ID 40074423
+
+Instructions:
+
+
+Credits:
+- 
---
+++ exercises/age-of-aquariums/js/script.js
@@ -0,0 +1,41 @@
+/**************************************************
+Exercise: Age of Aquariums
+Wayne Huras ID 40074423
+Brief:
+- Add a user-controlled shape (or image)
+- Make the user interact with the fish (or whatever they are in your simulation)
+- Change the fish (or whatever) creation
+- Add at least two endings
+
+Evaluation:
+- Functional (40%)
+- Stylish (20%)
+- Committed (20%)
+- Creative (20%)
+**************************************************/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
