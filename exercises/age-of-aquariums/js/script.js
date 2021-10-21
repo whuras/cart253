acba5d258c920ca260f21ddc32da8ae83017255a
Unknown <wayne.stamand@hotmail.com>
2021-10-20 20:34:38 -0400

    E4: Add base code
    Add base code from Pippin's YouTube video - 7.2 Introducing arrays

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -16,6 +16,9 @@ Evaluation:
 
 "use strict";
 
+let school = [];
+let schoolSize = 10;
+
 
 /**
 Description of preload
@@ -29,13 +32,68 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  for(let i = 0; i < schoolSize; i++){
+    let fish = createFish(random(0, width), random(0, height));
+    school.push(fish);
+  }
 }
 
+
+function createFish(x, y){
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 2
+  }
+  return fish;
+}
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  for(let i = 0; i < school.length; i++){
+    moveFish(school[i]);
+    displayFish(school[i]);
+  }
+}
+
+// chooses whether a fish changes directions and moves it
+function moveFish(fish){
+  // choose whether to change direction
+  let change = random(0, 1);
+  if(change < 0.05){
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+
+  // move the fish
+  fish.x = fish.x + fish.vx;
+  fish.y = fish.y + fish.vy;
+
+  // constrain the fish to the canvas
+  fish.x = constrain(fish.x, 0, width);
+  fish.y = constrain(fish.y, 0, height);
+}
+
+// displays the provided fish on the canvas
+function displayFish(fish){
+  push();
+  fill(200, 100, 100);
+  noStroke();
+  ellipse(fish.x, fish.y, fish.size);
+  pop();
+}
+
+// creates a fish at the mouse position
+function mousePressed(){
+  let fish = createFish(mouseX, mouseY);
+  school.push(fish);
 }
