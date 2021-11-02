7249c747feb1f75e8ee73eb12bfa70daf5fb2450
Unknown <wayne.stamand@hotmail.com>
2021-11-02 12:18:19 -0400

    E6: Started Make Some Noise exercise

---
+++ exercises/make-some-noise/README.md
@@ -0,0 +1,4 @@
+# CART253 - Creative Computation I, Exercise 6: Make Some Noise
+## Wayne Huras, ID 40074423
+
+Instructions:
---
+++ exercises/make-some-noise/js/script.js
@@ -0,0 +1,37 @@
+/**************************************************
+Exercise: Age of Aquariums
+Wayne Huras ID 40074423
+
+Explanation:
+
+
+Brief: Focus on working with p5.sound (sound files, oscillators, synthesizers, and audio input)
+
+
+**************************************************/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
