8b64bad58944ed82b41076f5ef29351317345ad3
Unknown <wayne.stamand@hotmail.com>
2021-11-06 18:31:42 -0400

    E6: Add Background Music
    Add background music: Crowander - Underwater

--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -2,22 +2,26 @@
 Exercise: Age of Aquariums
 Wayne Huras ID 40074423
 
+Brief:
+- Focus on working with p5.sound (sound files, oscillators, synthesizers, and audio input)
+- Ideally produce a prototype that explores how you will use sound in your final project
+
 Explanation:
-
-
-Brief: Focus on working with p5.sound (sound files, oscillators, synthesizers, and audio input)
-
+- Background song
+- Tone when the mouse is close to various objects (volume dependent on dist)
+- Sound effect when something happens
 
 **************************************************/
 
 "use strict";
 
+let bgMusic;
 
 /**
 Description of preload
 */
 function preload() {
-
+  bgMusic = loadSound("assets/sounds/Crowander - Underwater.mp3");
 }
 
 
@@ -25,7 +29,9 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(800, 600);
+  bgMusic.loop();
+  userStartAudio();
 }
 
 
@@ -33,5 +39,5 @@ function setup() {
 Description of draw()
 */
 function draw() {
-
+  background(128);
 }
