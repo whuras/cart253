6aa52312ec437ad6e43bec4b37962cad4537e68a
Unknown <wayne.stamand@hotmail.com>
2021-11-06 20:52:31 -0400

    E6: Add SoundEffects and Toggle Buttons
    Add sound effects for when the mouse is close to the target, add sound effects for when the target is clicked by the mouse, add toggle buttons for turning on/off the music and sound effect.

--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -7,7 +7,7 @@ Brief:
 - Ideally produce a prototype that explores how you will use sound in your final project
 
 Explanation:
-- Background song
+- Background music
 - Tone when the mouse is close to various objects (volume dependent on dist)
 - Sound effect when something happens
 
@@ -15,18 +15,23 @@ Explanation:
 
 "use strict";
 
-let bgMusic;
+let synth;
 
-let targets = [];
-let targetDiameter = 50;
-let targetColors = ["#000000", "#FFFFFF", "#272727", "#000000", "#FFFFFF"];
-let maxNumber = 5;
+let bgMusic;
+let musicIsPlaying = false;
+
+let soundEffect;
+
+let target;
+let targetSoundIsPlaying = false;
+let notes = ["C4", "E4", "G4", "A4"]; // to all be played at once
 
 /**
-Description of preload
+Preload music files
 */
 function preload() {
   bgMusic = loadSound("assets/sounds/Crowander - Underwater.mp3");
+  soundEffect = loadSound("assets/sounds/whoosh.wav");
 }
 
 
@@ -36,10 +41,19 @@ Description of setup
 function setup() {
   createCanvas(800, 600);
 
-  //bgMusic.loop();
-  userStartAudio();
+  synth = new p5.PolySynth();
 
-  createTargets(5);
+  // create target which hosts notes and sound effects
+  target = new Target(100, 100, 100, "#fff4d6", notes, soundEffect)
+
+  // create buttons for toggling music and sound
+  var buttonMusic = createButton("Toggle Music");
+  buttonMusic.mousePressed(toggleMusic);
+
+  var buttonSound = createButton("Toggle Sound");
+  buttonSound.mousePressed(toggleSound);
+
+  userStartAudio();
 }
 
 
@@ -47,22 +61,49 @@ function setup() {
 Description of draw()
 */
 function draw() {
-  background(128);
+  background("#eef1ef");
 
-  for(let i = 0; i < targets.length; i++){
-    targets[i].display();
+  // Music
+  if(musicIsPlaying){
+    bgMusic.loop();
+  }else{
+    bgMusic.stop();
+  }
+
+  // Display Objects
+  target.display();
+
+  // Play target sounds
+  if(targetSoundIsPlaying){
+    target.playNotes(synth);
+  }
+
+}
+
+
+/**
+When mouse is pressed, if the mouse is within the target it will play
+its sound effect.
+*/
+function mousePressed(){
+  let d = dist(mouseX, mouseY, target.x, target.y);
+  if(d < target.diameter / 2){
+    target.playSoundEffect();
   }
 }
 
 
-function createTargets(number){
+/**
+Toggles the music on and off
+*/
+function toggleMusic(){
+  musicIsPlaying = !musicIsPlaying;
+}
 
-  if(number > maxNumber) number = maxNumber;
 
-  let xVals = [100, 200, 300, 400, 500];
-  let yVals = [100, 200, 300, 400, 500];
-
-  for(let i = 0; i < number; i++){
-    append(targets, new Target(xVals[i], yVals[i], targetDiameter, targetColors[i]));
-  }
+/**
+Toggles the sound (velocity based on distance of mouse to target) on and off
+*/
+function toggleSound(){
+  targetSoundIsPlaying = !targetSoundIsPlaying;
 }
--- exercises/make-some-noise/js/target.js
+++ exercises/make-some-noise/js/target.js
@@ -7,17 +7,38 @@ Explanation:
 **************************************************/
 
 class Target{
-  constructor(x, y, diameter, color){
+  constructor(x, y, diameter, color, notes, soundEffect){
     this.x = x;
     this.y = y;
     this.diameter = diameter;
     this.color = color;
+    this.notes = notes;
+    this.soundEffect = soundEffect;
   }
 
   display(){
     push();
+    noStroke();
     fill(this.color);
     circle(this.x, this.y, this.diameter);
     pop();
   }
+
+  playNotes(synth){
+
+    let d = dist(this.x, this.y, mouseX, mouseY);
+    let velocity = map(d, 0, width, 0.2, 0);
+
+    if(frameCount % 2 == 0){
+      synth.play(notes[0], velocity, 0, 1/60);
+      synth.play(notes[1], velocity, 0, 1/60);
+      synth.play(notes[2], velocity, 0, 1/60);
+      synth.play(notes[3], velocity, 0, 1/60);
+    }
+  }
+
+  playSoundEffect(){
+    soundEffect.play();
+  }
+
 }
