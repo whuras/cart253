cc05e4fc8787603935442eb627c3acfd1bb76143
Unknown <wayne.stamand@hotmail.com>
2021-11-08 12:55:50 -0500

    P2: Setup Project 2
    Basic project setup, comments/readme, and pdf proposal

---
+++ projects/project2/prototype/README.md
@@ -0,0 +1,8 @@
+# CART253 - Creative Computation I, Project 2: Anything
+## Wayne Huras, ID 40074423
+
+Instructions:
+
+
+Credits:
+- Pulse code: https://editor.p5js.org/dansakamoto/sketches/H1ICcXXtm
---
+++ projects/project2/prototype/js/script.js
@@ -0,0 +1,38 @@
+/**************************************************
+Project 2: Anything
+Wayne Huras ID 40074423
+
+Brief: Part 1 - Proposal (see pdf) and Prototype
+- A small experiment that explores one or more ideas that will be fully implemented in the final project
+- The key is that is should give you a chance to make progress on the project code
+- Roughly the scale of one of the exercises
+
+Explanation:
+
+**************************************************/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
